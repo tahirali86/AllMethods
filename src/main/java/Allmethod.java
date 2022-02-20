1b48623073cf72b3e2b939133cbe72d95b8d9899
tahir.ali3 <tahirali86@gmail.com>
2022-02-20 17:53:02 +0500

    All Methods Program

---
+++ src/main/java/Allmethod.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Allmethod {
+
+    public static void main(String args[]){
+
+        long EvenOdd= EvenOdd(6);
+        System.out.println(EvenOdd);
+        AtoZ();
+        Concatinate();
+    }
+
+    static long EvenOdd(int n) {
+        int num;
+        System.out.println("Enter an Integer number:");
+
+        //The input provided by user is stored in num
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        /* If number is divisible by 2 then it's an even number
+         * else odd number*/
+        if ( num % 2 == 0 )
+            System.out.println("Entered number is even");
+        else
+            System.out.println("Entered number is odd");
+        return 0;
+    }
+
+
+
+    static void AtoZ() {
+
+
+        char c;
+
+        for(c = 'A'; c <= 'Z'; ++c)
+            System.out.print(c + " ");
+    }
+    static void Concatinate() {
+        String s1="Tahir";
+        String s2="Ali";
+        String s3=s1.concat(s2);
+        System.out.println(s3);
+
+    }
+
+
+}
+
